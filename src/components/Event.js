b7864a456b70e831bb649b6a30b9f24a78ddebb3
vskelton <vskelton25@gmail.com>
2025-02-05 10:16:34 -0500

    update event JS file

--- src/components/Event.js
+++ src/components/Event.js
@@ -10,8 +10,8 @@ const Event = ({ event }) => {
       <p>{event && event.location}</p>
       <p>{event && (new Date(event.created)).toUTCString()}</p>
       {showDetails ?
-      <p className="details">{event && event.description}</p> :
-      null
+        <p className="details">{event && event.description}</p> :
+        null
       }
       <button className="details-btn" onClick={() => {
         showDetails ? setShowDetails(false) : setShowDetails(true)
@@ -19,6 +19,5 @@ const Event = ({ event }) => {
     </li>
   )
 }
-
 
 export default Event;
