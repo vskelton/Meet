1ae2ec3def861d90985e8741028bdb7f03ba17e9
vskelton <vskelton25@gmail.com>
2025-02-05 10:18:35 -0500

    update number of events JS file

--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,23 +1,26 @@
+// src/components/NumberOfEvents.js
+
 import { useState } from "react";
 
 const NumberOfEvents = ({ }) => {
+
   const [number, setNumber] = useState(32);
 
   const handleInputChanged = (event) => {
     const value = event.target.value;
     setNumber(value);
-  };
+  }
 
   return (
     <div id="number-of-events">
       <label htmlFor="number-of-events-input">Number of Events: </label>
-      <input 
+      <input
         type="text"
         id="number-of-events-input"
         className="number-of-events-input"
         value={number}
         onChange={handleInputChanged}
-        />
+      />
     </div>
   );
 }
