b06a353da1643e2704b34883204078cde2f8547a
vskelton <vskelton25@gmail.com>
2025-02-05 10:20:04 -0500

    update app JS file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
+// src/App.js
+
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
+import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
+
 import './App.css';
-import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [events, setEvents] = useState([]);
-  const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [events, setEvents] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
 
   useEffect(() => {
@@ -21,8 +23,8 @@ const App = () => {
     const filteredEvents = currentCity === "See all cities" ?
       allEvents :
       allEvents.filter(event => event.location === currentCity)
-      setEvents(filteredEvents.slice(0, currentNOE));
-      setAllLocations(extractLocations(allEvents));
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
   }
 
   return (
