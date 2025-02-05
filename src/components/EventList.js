ae7b7c48c968ecf6db7f645337aacae9d3ac1de0
vskelton <vskelton25@gmail.com>
2025-02-05 10:17:29 -0500

    update event list JS file

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -5,8 +5,8 @@ import Event from "./Event";
 const EventList = ({ events }) => {
   return (
     <ul id="event-list">
-      {events ? 
-        events.map(event => <Event key={event.id} event={event} />)  : 
+      {events ?
+        events.map(event => <Event key={event.id} event={event} />) :
         null}
     </ul>
   );
