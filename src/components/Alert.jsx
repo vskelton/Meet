15dff3a764f6f1343dfeb46557ccea31fc2d444a
vskelton <vskelton25@gmail.com>
2025-03-03 12:05:07 -0500

    Correcting spelling mistakes

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -12,10 +12,10 @@ class Alert extends Component {
   class InfoAlert extends Alert {
     constructor(props) {
       super(props);
-      this.color = 'rgb(0, 0, 225)';
+      this.color = 'rgb(0, 0, 255)';
       this.bgColor = 'rgb(220, 220, 255)';
     }
-  }
+   }
 
   getStyle = () => {
     return {
@@ -39,5 +39,6 @@ class Alert extends Component {
         <p style={this.getStyle()}>{this.props.text}</p>
       </div>
     );
-  }
+  };
 }
+export { InfoAlert };
