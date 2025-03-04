1b4b70155dde62a317626aea4a51831d2d71b492
vskelton <vskelton25@gmail.com>
2025-03-04 11:38:04 -0500

    Created manifest.json file

--- meet/vite.config.js
+++ meet/vite.config.js
@@ -1,7 +1,62 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/',
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        "short_name": "React App",
+        "name": "Create React App Sample",
+        "icons": [
+          {
+            "src": "favicon.ico",
+            "sizes": "48x48",
+            "type": "image/x-icon",
+            "purpose": "maskable"
+          },
+          {
+            "src": "meet-app-144.png",
+            "type": "image/png",
+            "sizes": "144x144",
+            "purpose": "any"
+          },
+          {
+            "src": "meet-app-192.png",
+            "type": "image/png",
+            "sizes": "192x192",
+            "purpose": "maskable"
+          },
+          {
+            "src": "meet-app-512.png",
+            "type": "image/png",
+            "sizes": "512x512",
+            "purpose": "maskable"
+          }
+        ],
+        "start_url": ".",
+        "display": "standalone",
+        "theme_color": "#000000",
+        "background_color": "#ffffff"
+      },
+      srcDir: 'src',
+      filename: 'service-worker.js',
+      registerType: 'autoUpdate',
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /\/.*\.png$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'images',
+              expiration: {
+                maxEntries: 50,
+              },
+            },
+          },
+        ],
+      },
+    })],
 })
