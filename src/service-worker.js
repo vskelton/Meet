31d7d918c3f10088a1d3d508a5336968274ffb77
vskelton <vskelton25@gmail.com>
2025-03-04 11:47:43 -0500

    update to service-worker.js file

--- src/service-worker.js
+++ src/service-worker.js
@@ -25,25 +25,16 @@ precacheAndRoute(self.__WB_MANIFEST);
 // are fulfilled with your index.html shell. Learn more at
 // https://developers.google.com/web/fundamentals/architecture/app-shell
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+const handler = createHandlerBoundToURL('/index.html');
+
 registerRoute(
-  // Return false to exempt requests from being fulfilled by index.html.
   ({ request, url }) => {
-    // If this isn't a navigation, skip.
-    if (request.mode !== 'navigate') {
-      return false;
-    } // If this is a URL that starts with /_, skip.
-
-    if (url.pathname.startsWith('/_')) {
-      return false;
-    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
-
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    } // Return true to signal that we want to use the handler.
-
+    if (request.mode !== 'navigate') return false;
+    if (url.pathname.startsWith('/_')) return false;
+    if (url.pathname.match(fileExtensionRegexp)) return false;
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+  handler
 );
 
 // An example runtime caching route for requests that aren't handled by the
